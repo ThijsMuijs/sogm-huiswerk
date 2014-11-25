5d145201d4ce4f7d294b7c116c3fc79662c29ad0
Thijs Muijs <thijsmuijs@hotmail.com>
2014-11-25 16:54:44 +0100

    functies opdracht

---
+++ C++ Functies/MaxArray.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int maxArray(int a[], int size){
+int max = 0;
+for (int i = 0; i < size ; i++){
+	if( a[i] > max)
+	{max = a[i];}
+  }
+return max;
+}
+
+int main(){
+
+int size;
+cout << "geef grootte van array" << endl;
+cin >> size;
+
+int array[size];
+for(int i = 0; i < size; i++){
+array[i] = rand() % 1000;
+}
+
+cout << "hoogste getal in de array is: " << maxArray(array, size) << endl;
+return 0;
+}
